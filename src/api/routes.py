dabd28a74fac124b80c7cd555b1f02e99e66091b
cpozo94 <cpg.948@gmail.com>
2023-06-05 06:23:03 +0000

    updating files

--- src/api/routes.py
+++ src/api/routes.py
@@ -30,3 +30,53 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return jsonify(new_user.serialize()), 201
+
+
+#login 
+@api.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    token = Controller.login(body)
+    if token.get('token'):
+        return jsonify(token),200
+    return jsonify(token),token['status']
+
+def login(body):
+    user_verify = verify_user_email_and_pass(body)
+    if user_verify.get('error') is not None:
+        return user_verify
+    user = Repository.get_user_by_email(body['email'])
+    if user is None:
+        return {"msg": "User not found", "error": True, "status": 404}
+    if bcrypt.checkpw(body['password'].encode(), user.password.encode()):
+        user_serialize = user.serialize()
+        new_token = create_access_token(identity=user.serialize())
+        rol = user_serialize['user_rol']['rol_type']
+        return {"token": new_token,"rol": rol}
+    return {"msg": "User not found", "error": True, "status": 404 }  
+
+
+#register_client
+
+@api.route('/register/client', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    user = Controller.create_user(body,"client")
+    if isinstance(user, User):
+        return jsonify(user.serialize()), 200
+    return jsonify(user)
+
+def create_user(new_user,rol_type):
+    user_verify = verify_user_email_and_pass(new_user)
+    if user_verify.get('error') is not None:
+        return user_verify
+    hashed = bcrypt.hashpw(new_user['password'].encode(), bcrypt.gensalt(14))
+    user_rol_id = User_rol.query.filter_by(rol_type=rol_type).first()
+
+    return Repository.create_user(new_user['email'], hashed.decode(), new_user['name'], new_user['last_name'], user_rol_id.id)
+
+    def create_user(email, password, name, last_name, user_rol_id):
+    new_user = User(email = email, password=password, name=name, last_name= last_name, user_rol_id = user_rol_id)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
